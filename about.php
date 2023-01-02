223ea1c27224cb9dec34c683c125659ab11d1d5f
Pandurang3 <naikpandurang3@gmail.com>
2023-01-02 15:00:46 +0530

    first commit

--- about.php
+++ about.php
@@ -5,5 +5,14 @@
     <p class="text-center">
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore impedit totam porro iure reiciendis autem possimus sapiente, optio, exercitationem ipsum assumenda mollitia, recusandae expedita culpa ratione voluptatem esse quos quam?
     </p>
+    <p class="text-center">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore impedit totam porro iure reiciendis autem possimus sapiente, optio, exercitationem ipsum assumenda mollitia, recusandae expedita culpa ratione voluptatem esse quos quam?
+    </p>
+    <p class="text-center">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore impedit totam porro iure reiciendis autem possimus sapiente, optio, exercitationem ipsum assumenda mollitia, recusandae expedita culpa ratione voluptatem esse quos quam?
+    </p>
+    <p class="text-center">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore impedit totam porro iure reiciendis autem possimus sapiente, optio, exercitationem ipsum assumenda mollitia, recusandae expedita culpa ratione voluptatem esse quos quam?
+    </p>
 
 <?php include 'inc/footer.php'; ?>
